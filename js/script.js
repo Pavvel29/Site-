e414824311db6fa3c1b2185dcaaac62098d142f4
Pavvel29 <84814467+Pavvel29@users.noreply.github.com>
2021-06-10 00:14:11 +0300

    New Site

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+/*
+const ElementScrol = document.querySelector('.block-slider__rectangle');
+ElementScrol.addEventListener('mousedown', function(){
+    console.log(111);
+    this.addEventListener('mouseup', function(){
+    console.log(222);
+    });
+});
+*/
+
+/*
+const scrollSpase = document.querySelector('.block-slider__working');
+const scrollItem = document.querySelector('.block-slider__rectangle');
+
+console.log(scrollItem.offsetParent);
+
+const scrollSpaseWidth = scrollSpase.offsetWidth;
+const scrollSpaseHeight = scrollSpase.offsetHeight;
+
+console.log(scrollSpaseWidth);
+console.log(scrollSpaseHeight);
+
+const scrollItemWidth = scrollItem.offsetWidth;
+const scrollItemHeight = scrollItem.offsetHeight;
+
+const scrollItemLeft = scrollItem.offsetLeft;
+
+console.log(scrollItemWidth);
+console.log(scrollItemHeight);
+console.log(scrollItemLeft);
+let triger = false;
+
+function test(){
+     window.addEventListener('mousemove', function(){
+     console.log(window.event.clientX);    
+   });     
+}
+
+scrollItem.addEventListener('mousedown',test);
+*/
+
+
+document.addEventListener('keydown', function(){
+   if(event.code == 'KeyZ'){
+   	console.log('you clik key z');
+   }
+   return;
+});
+
+
+
+
+
+
+
+
+	
+     
+     	
+     	
+     
+    
+   
+
+	
+
+
+
+
